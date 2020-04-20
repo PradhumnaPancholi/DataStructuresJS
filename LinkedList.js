c98944a80a6ba7aa45121a3be46efa181183ffaa
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-04-20 04:42:10 -0400

    added remove method

--- LinkedList.js
+++ LinkedList.js
@@ -25,7 +25,7 @@ class LinkedList {
             //if yes, add new node as first element//
             this[head] = newNode
             //print the LinkedList//
-            console.log('LinkedList: ', this[head])
+            console.log(`Added ${data} into the list`)
         }else{
             //start looking for empty/last node//
             let current  = this[head]
@@ -36,7 +36,7 @@ class LinkedList {
             //assign the 'newNode' to 'next'
             current.next = newNode
             //print the LinkedList//
-            console.log('LinkedList: ', this[head])
+            console.log(`Added ${data} into the list`)
         }
     }
 
@@ -48,19 +48,65 @@ class LinkedList {
             let current = this[head]
             //to keep track of index//
             let i = 0
-            //check if the point is on empty or undefined index//
+            //iterate through list until to get till index or end(undefined)//
             while((current !== null) && (i < index) ){
                 current = current.next
                 i = i+1
             }
-            //return if the index is non-null//
-            return console.log(current.value)
+            //return if the index of non-null in not undefined ie. The value at given index unless it's out of range(undefined))//
+            return current !== null ? console.log(`Value of index ${index}: ${current.value}`) : undefined
         }
         else{
-            //throw error for undefined index
+            //return undefined for no existing index
             console.log('LinkedList: ', this[head])
             return undefined
         }
+    }
+    //function to remove a node//
+    remove(index) {
+    //take care od special cases: [1]. Empty list, [2]. 0 as in input index, [3]. Index out of range
+
+        //operation can't be performed if idex is negave or the list is empty//
+        if((this[head] === null) || (index < 0)){
+            throw new RangeError(`The ${index} is out of range`)
+        }
+        //operation to remove the first node(head) if there is only one node present in array//
+        if(index === 0) {
+            //store data in temp variable (to show in response)
+            let data = this[head].value
+            // replace the data in 'head' with 'next'(empty)//
+            this[head] = this[head].next
+            //respond with a msg//
+            console.info(`Deleted ${data} at index ${index}`)
+            return
+        }
+    //operation to perform find and remove node from rest of list//
+        
+        //pointer to go through list//
+        let current = this[head]
+        //to keep track of node before current in loop (to remove data from 'next)
+        let previous = null
+        //to keep track of index//
+        let i = 0
+        //pass through list until at index or end
+        while((current !== null) && (index > i)){
+            //store the value of current//
+            previous = current
+            //increment to keep going//
+            current = current.next
+            i = i+1
+        }
+        //if node is found, remove data//
+        if(current !== null){
+            //skip over node to remove 
+            //i.e remove the value of found index from 'previous.next' and attach the value to index after 'current'(the one we are removing)//
+            previous.next = current.next
+            //respond with msg//
+            console.info(`Succesfully deleted ${current.value} at index ${index}`)
+            return
+        } 
+        //if node is not found, throw error for invalid index//
+        throw new RangeError(`Index ${index} doesn't exist on list`)
     }
 }
 
@@ -72,5 +118,7 @@ myList.add(4)
 myList.add(5)
 //-----------//
 myList.get(1)
+//-----------//
+myList.remove(3)
 
 
