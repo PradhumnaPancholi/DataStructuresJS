4a9f769b6270fa8dd04b0848017fe93535ad38de
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-04-20 00:44:44 -0400

    added better object oriented implementation with 'add' method

--- LinkedList.js
+++ LinkedList.js
@@ -4,21 +4,54 @@ class Node {
         this.next = null
     }
 }
-//creating first node//
-const head = new Node(1)
-console.log('Chain: ', head)
+// //creating first node//
+// const head = new Node(1)
+// console.log('Chain: ', head)
 
-//creating and  adding second node//
-head.next = new Node(2)
-console.log('Chain', head)
+// //creating and  adding second node//
+// head.next = new Node(2)
+// console.log('Chain', head)
 
-//creating third node//
-head.next.next = new Node(3)
-console.log('Chain', head)
+// //creating and adding third node//
+// head.next.next = new Node(3)
+// console.log('Chain', head)
 
-//adding value of 'next' in nodes to ck//
-// head.next = node2
-// node2.next = node3
+const head = Symbol('head')
 
-//printing the chain of nodes//
-// console.log(head)
+class LinkedList {
+
+    //starts as empty || null
+    constructor(){
+        this[head] = null
+    }
+
+    //function to add a node into LinkedList//
+    add(data){
+        
+        //create a "Node" with given data//
+        let newNode = new Node(data)
+
+        //check if the list is empty//  
+        if(this[head] === null){
+            //if yes, add new node as first element//
+            this[head] = newNode
+            //print the LinkedList//
+            console.log('LinkedList: ', this[head])
+        }else{
+            //start looking for empty/last node//
+            let current  = this[head]
+            //follow the 'next' trail //
+            while(current.next !== null){
+                current = current.next
+            }
+            //assign the 'newNode' to 'next'
+            current.next = newNode
+            //print the LinkedList//
+            console.log('LinkedList: ', this[head])
+        }
+    }
+}
+
+myList = new LinkedList()
+myList.add(1)
+myList.add(2)
