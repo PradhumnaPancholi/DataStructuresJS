12a777fb168fa7838e76841e2dc270b0700a8212
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-04-21 06:27:03 -0400

    implemented basic BST

---
+++ BinarySearchTree.js
@@ -0,0 +1,16 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinarySearchTree{
+    constructor(value){
+        this.root = new Node(value)
+        this.count = 0
+    }
+
+    
+}
